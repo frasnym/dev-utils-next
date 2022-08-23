3bee7a36c67e0ed82fabe205fec7fa638efe9ca3
FrasNym <frastyawan.nym@gmail.com>
2022-08-23 16:34:08 +0800

    💄 radio-button: Update css

--- components/ui/RadioButton/RadioButton.tsx
+++ components/ui/RadioButton/RadioButton.tsx
@@ -12,19 +12,21 @@ const RadioButton: NextPage<RadioButtonProps> = (props) => {
   return (
     <div className="flex">
       {props.radios.map((radio) => (
-        <div key={radio.value} className="flex items-center mr-4">
+        <label
+          key={radio.value}
+          htmlFor={radio.value}
+          className="flex cursor-pointer rounded-lg px-5 py-2.5 text-sm m-2 focus:z-10 focus:ring-4 focus:outline-none text-gray-900 bg-white border border-gray-300 hover:bg-gray-100 focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        >
           <input
-            checked={radio.isChecked}
             type="radio"
+            checked={radio.isChecked}
             value={radio.value}
+            id={radio.value}
             name={props.name}
-            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
             onChange={radio.onChange}
           />
-          <label className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-            {radio.label}
-          </label>
-        </div>
+          <span className="pl-2">{radio.label}</span>
+        </label>
       ))}
     </div>
   );
